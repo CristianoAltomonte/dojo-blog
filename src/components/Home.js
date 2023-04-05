a344a86cf258b5799c595f9aec25f23af852011d
cristiano altomonte <cristianoaltomonte@hotmail.it>
2023-04-05 13:38:27 +0100

    props with data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,43 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 function Home() {
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState(25);
+
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new Website",
+      body: "lorem ipsum dolor sit amet, consectetur adip",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome to the Party",
+      body: "lorem ipsum dolor sit amet, consectetur adip",
+      author: "cristiano",
+      id: 2,
+    },
+    {
+      title: "My new Website",
+      body: "lorem ipsum dolor sit amet, consectetur adip",
+      author: "andrea",
+      id: 3,
+    },
+  ]);
+
   const handleClick = () => {
-    alert("ciao cristiano");
+    setName("luigi");
+    setAge(30);
   };
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
+      <p>
+        {name} is {age} years old
+      </p>
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 }
