848d2e89fd6dc52bd6210734368f13873acde3fc
cristiano altomonte <cristianoaltomonte@hotmail.it>
2023-04-05 16:31:28 +0100

    Fetch Errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ import BlogList from "./BlogList";
 function Home() {
   const [name, setName] = useState("mario");
   const [age, setAge] = useState(25);
-
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -20,15 +21,29 @@ function Home() {
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          // error coming back from server
+          throw Error("could not fetch the data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
+        setPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        // auto catches network / connection error
+        setPending(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
       <p>
