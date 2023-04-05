ac89d0d8f289bf6f22964e8e9083739e53a1aa72
cristiano altomonte <cristianoaltomonte@hotmail.it>
2023-04-05 12:03:16 +0100

    bottone in un componente

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 function Home() {
+  const handleClick = () => {
+    alert("ciao cristiano");
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
