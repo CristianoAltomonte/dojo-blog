4699012bda2d45b211fcd574e7149f032bd7f8b0
cristiano altomonte <cristianoaltomonte@hotmail.it>
2023-04-06 10:50:52 +0100

    Controlled Inputs (forms)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "../UseFetch";
 
@@ -8,18 +8,18 @@ function Home() {
     isPending,
     error,
   } = useFetch("http://localhost:3000/blogs");
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(25);
+  // const [name, setName] = useState("mario");
+  // const [age, setAge] = useState(25);
 
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter((blog) => blog.id !== id);
   //   setBlogs(newBlogs);
   // };
 
-  const handleClick = () => {
-    setName("luigi");
-    setAge(30);
-  };
+  // const handleClick = () => {
+  //   setName("luigi");
+  //   setAge(30);
+  // };
 
   return (
     <div className="home">
@@ -27,10 +27,10 @@ function Home() {
       {isPending && <div>Loading...</div>}
 
       <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
+      {/* <button onClick={handleClick}>Click me</button>
       <p>
         {name} is {age} years old
-      </p>
+      </p> */}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
